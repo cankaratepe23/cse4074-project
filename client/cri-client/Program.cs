fd739ec0f141ae41e536d8f836550606c6351973
Sinem C. Kontaş <sinemkontas@gmail.com>
2021-12-27 20:03:54 +0300

    create initial client project structure
    closes #2

---
+++ client/cri-client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace cri_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
