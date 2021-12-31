ea1ba85d6419ec7f3078ec4a98a730d67af87b87
Can Karatepe <cankaratepe2000@gmail.com>
2021-12-31 18:51:38 +0300

    Create initial server project structure.

---
+++ server/cri-server/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace cri_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+    }
+}
