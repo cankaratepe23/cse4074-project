3b9b95b0a2df5d663552712db102389c8560e5a9
Can Karatepe <cankaratepe2000@gmail.com>
2022-01-01 22:01:18 +0300

    Change default namespace and add initial UserService implementation.

--- server/CriServer/UserService.cs
+++ server/CriServer/UserService.cs
@@ -1,17 +1,26 @@
-﻿using CriServer;
+﻿using CriServer.IServices;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cri_server
+namespace CriServer
 {
-    class UserService
+    class UserService : IUserService
     {
+        private readonly CriContext _criContext;
+        
+        public UserService(IServiceProvider services)
+        {
+            _criContext = services.GetService<CriContext>();
+        }
+
         public void RegisterUser(User newUser)
         {
-            
+            _criContext.Add<User>(newUser);
+            _criContext.SaveChanges();
         }
     }
 }
