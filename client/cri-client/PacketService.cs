3e98b3961fe2e092bf7280d4a313ddf94f98a498
Sinem C. Kontaş <sinemkontas@gmail.com>
2022-01-05 00:19:00 +0300

    Packet Service Implemented

--- client/cri-client/PacketService.cs
+++ client/cri-client/PacketService.cs
@@ -13,12 +13,14 @@ namespace cri_client
         const int USERNAME_MAX_LENGTH = 16;
         const int PASSWORD_MAX_LENGTH = 16;
         const int TCP_PORT = 5555;
+        const int UDP_PORT = 5556;
         const string SERVER = "192.168.1.24";
+        const int MESSAGE_MAX_LENGTH = 325;
+        const int MAX_USER_COUNT = 100;
 
         public void SendPacket(bool isUdp, string payload)
         {
-            //int port = 5555;
-            //string server = "192.168.1.24";
+            
             Byte[] data = System.Text.Encoding.UTF8.GetBytes(payload);
             if (!isUdp)
             {
@@ -29,10 +31,12 @@ namespace cri_client
 
                 stream.Close();
             }
-            
-           else
+
+            else
             {
-                //udp gönder
+                UdpClient udpClient = new UdpClient();
+                udpClient.Send(data, data.Length, SERVER, UDP_PORT);
+
             }
 
         }
@@ -40,18 +44,113 @@ namespace cri_client
         {
             if (username.Length <= USERNAME_MAX_LENGTH && password.Length <= PASSWORD_MAX_LENGTH)
             {
-                string packet = ProtocolCode.Register +"\n" + username + "\n" + password;
+                string packet = ProtocolCode.Register + "\n" + username + "\n" + password;
                 //string packet = $"00\n{username}\n{password}";
                 SendPacket(false, packet);
             }
             else
             {
-                throw new Exception("username or password is longer than 16 characters");
+                throw new Exception("username or password char limit exceeded");
             }
 
 
         }
 
-        
+        public void Login(string username, string password)
+        {
+            if(username.Length <= USERNAME_MAX_LENGTH && password.Length <= PASSWORD_MAX_LENGTH)
+            {
+                string packet = ProtocolCode.Login + "\n" + username + "\n" + password;
+                SendPacket(false, packet);
+            }
+            else
+            {
+                throw new Exception("username or password char limit exceededvvvv");
+            }
+        }
+
+        public void Logout()
+        {
+            string packet = ProtocolCode.Logout.ToString();
+            SendPacket(false, packet);
+        }
+
+        public void Hello(string username)
+        {
+            if(username.Length <= USERNAME_MAX_LENGTH)
+            {
+                string packet = ProtocolCode.Hello + "\n" + username;
+                SendPacket(true, packet);
+            }
+            else
+            {
+                throw new Exception("username char limit exceeded");
+            }
+        }
+
+        public void Search(string username)
+        {
+            if(username.Length <= USERNAME_MAX_LENGTH)
+            {
+                string packet = ProtocolCode.Search + "\n" + username;
+                SendPacket(false, packet);
+            }
+            else
+            {
+                throw new Exception("username char limit exceeded");
+            }
+        }
+
+        public void Chat()
+        {
+            string packet = ProtocolCode.Chat.ToString();
+            SendPacket(false, packet);
+        }
+
+        public void Text(string username, string message)
+        {
+            if(username.Length <= USERNAME_MAX_LENGTH && message.Length <= MESSAGE_MAX_LENGTH)
+            {
+                string packet = ProtocolCode.Text + "\n" + username + "\n" + message;
+                SendPacket(false, packet);
+            }
+            else
+            {
+                throw new Exception("username or message char limit exceeded");
+            }
+        }
+
+        public void GroupCreate(List<string> usernames)
+        {
+            if(usernames.Count <= MAX_USER_COUNT)
+            {
+                string packet = ProtocolCode.GroupCreate + "\n" + String.Join("\n", usernames);
+                SendPacket(false, packet);
+            }
+            else
+            {
+                throw new Exception("user count limit exceeded");
+            }
+        }
+
+        public void GroupSearch(Guid gid)
+        {
+            string packet = ProtocolCode.GroupSearch + "\n" + gid;
+            SendPacket(false, packet);
+        }
+
+        public void GroupText(Guid gid, string username, string message)
+        {
+            if (username.Length <= USERNAME_MAX_LENGTH && message.Length <= MESSAGE_MAX_LENGTH)
+            {
+                string packet = ProtocolCode.GroupText + "\n" + gid + "\n" + username + "\n" + message;
+                SendPacket(false, packet);
+            }
+            else
+            {
+                throw new Exception("username or message char limit exceeded");
+            }
+        }
+
     }
 }
