eab1663088701904b20dc8532754aa3994b3bc77
Sinem C. Kontaş <sinemkontas@gmail.com>
2022-01-05 23:23:35 +0300

    Client Initial UI

--- client/CriClient/Program.cs
+++ client/CriClient/Program.cs
@@ -1,11 +1,41 @@
-﻿namespace CriClient
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CriClient
 {
     class Program
     {
+
+        static bool packetsent = false;
         static void Main(string[] args)
         {
+        
             PacketService packetservice = new PacketService();
-            packetservice.Register("sinem", "123");
+            Console.WriteLine("1.Register\n2.Login");
+            string menuopt = Console.ReadLine();
+            menuopt = menuopt.ToLower();
+            Console.WriteLine("Type username of max. 16 characters");
+            string uname = Console.ReadLine();
+            Console.WriteLine("Type password of max. 16 characters");
+            string pword = Console.ReadLine();
+
+
+            if (menuopt == "1" || menuopt == "register")
+            {
+                packetservice.Register(uname, pword);
+                packetsent = true;
+            }
+            else
+            {
+                packetservice.Login(uname, pword);
+                packetsent = true;
+            }
+
+            //packetservice.ReceivePacket();
+
 
         }
     }
