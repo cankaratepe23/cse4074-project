3d9fd14b3f73c2fa5e1109c98b9ac0860f40070a
Eymen Topçuoğlu <eymentopcuoglu52@gmail.com>
2022-01-11 01:36:40 +0300

    Remove redundant protocol codes for Registry

--- server/CriServer/ProtocolCode.cs
+++ server/CriServer/ProtocolCode.cs
@@ -16,8 +16,6 @@ namespace CriServer
         public static ProtocolCode Logout => new("02");
         public static ProtocolCode Hello => new("03");
         public static ProtocolCode Search => new("04");
-        public static ProtocolCode Chat => new("05");
-        public static ProtocolCode Text => new("06");
         public static ProtocolCode GroupCreate => new("07");
         public static ProtocolCode GroupSearch => new("08");
         public static ProtocolCode GroupText => new("09");
